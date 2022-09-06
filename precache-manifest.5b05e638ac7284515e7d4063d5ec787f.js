9cd3056acc025484881fe70abf948e0e82cb2351
Rohan086 <20dcs086@charusat.edu.in>
2022-09-06 17:36:16 +0530

    Updates

---
+++ precache-manifest.5b05e638ac7284515e7d4063d5ec787f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944d0982115f398c9019bee7c2fc2c12",
+    "url": "/Rohan_20DCS086/index.html"
+  },
+  {
+    "revision": "44f65ac15abf3e84ef50",
+    "url": "/Rohan_20DCS086/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "86a68ff484708dd88cbb",
+    "url": "/Rohan_20DCS086/static/css/main.48551862.chunk.css"
+  },
+  {
+    "revision": "44f65ac15abf3e84ef50",
+    "url": "/Rohan_20DCS086/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Rohan_20DCS086/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a68ff484708dd88cbb",
+    "url": "/Rohan_20DCS086/static/js/main.89810267.chunk.js"
+  },
+  {
+    "revision": "4397dca7189b47796b12",
+    "url": "/Rohan_20DCS086/static/js/runtime-main.7254242e.js"
+  }
+]);
